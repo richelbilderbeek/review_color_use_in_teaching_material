4b4be4693fa0f2f2b3cf9dc88d2f30826f87c301
richelbilderbeek <richel@richelbilderbeek.nl>
2023-12-21 09:31:19 +0100

    Use script

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/// Creates a mermaid flowchart that shows a certain color scheme
+
+#include <cassert>
+
+/// Creates the text of a mermaid flowchart
+/// that shows a certain color scheme
+std::vector<std::string> create_text() {
+
+}
+
+void test() {
+  // Must have text
+  {
+    assert(create_text());
+  }
+}
+
+int main() {
+
+}
